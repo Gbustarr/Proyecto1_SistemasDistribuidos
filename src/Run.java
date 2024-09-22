5e537246f65653839bfeb1569f7be8118d1b5d48
Gbustarr <oknmil@gmail.com>
2024-09-21 23:00:49 -0300

    ref(run): Cambiada la logica para detectar el SO

--- src/Run.java
+++ src/Run.java
@@ -7,60 +7,45 @@ public class Run {
     public static void main(String[] args) {
         System.out.println("Papu run");
 
-        String imagePath = "img\\tipo_Ruido_40.0.png"; // Windows
-        String imagePath2 = "src/images/img_1.png"; // Linux
+        // Para detectar el tipo de sistema operativo
+        String osName = System.getProperty("os.name").toLowerCase();
+        String imagePath;
+        String savePath;
+
+        if (osName.contains("win")) {
+            imagePath = "img\\tipo_Ruido_40.0.png"; // Windows
+            savePath = "img\\Prueba1.png";
+        } else {
+            imagePath = "src/images/img_1.png"; // Linux
+            savePath = "img/Prueba1.png";
+        }
+
         File imageFile = new File(imagePath);
-        File imageFile2 = new File(imagePath2);
 
         ImageLoader imgLoader = null;
         Image img = null;
-        
+
         if (imageFile.exists()) {
             try {
                 imgLoader = new ImageLoader(imagePath);
                 img = imgLoader.getImage();
 
-                img.setOS(0);
+                img.setOS(osName.contains("win") ? 0 : 1);
                 img.setRGBChannels();
 
                 ImageProcessor processor = new ImageProcessor(img);
-                
-                //Eroder paralelo
-                processor.processEroderP("img/Prueba1.png",3);
-                
+
+                // Eroder paralelo
+                processor.processEroderP(savePath, 3);
+
                 // EroderSecuencial
-                //processor.processEroderS("img/tipo_Erocionada.png",1);
+                // processor.processEroderS("img/tipo_Erocionada.png", 1);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
-            if(imageFile2.exists()){
-                try {
-                    System.out.println("Image file exists: " + imageFile2.getAbsolutePath());
-                    imgLoader = new ImageLoader(imagePath2);
-                    System.out.println(imgLoader.getImage().getAncho());
-
-                    img = imgLoader.getImage();
-
-                    img.setOS(1);
-
-                    img.setRGBChannels();
-
-                    img.saveRGBtoImages();
-                    ImageProcessor processor = new ImageProcessor(img);
-                
-                    //Eroder paralelo
-                    processor.processEroderP("img/Prueba1.png",3);
-                    
-                    // EroderSecuencial
-                    //processor.processEroderS("img/tipo_Erocionada.png",1);
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                    }
-            }else{
-                System.out.println("Image file does not exist");
-            }
+            System.out.println("Image file does not exist");
         }
     }
 }
